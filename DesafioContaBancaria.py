f173677f4af1389f22448cce3d42b730de92ed0b
Yago Gama <yagoggb97@gmail.com>
2024-11-14 04:58:32 -0300

    Adicionado função para saque e deposito

--- DesafioContaBancaria.py
+++ DesafioContaBancaria.py
@@ -1,6 +1,37 @@
 from datetime import datetime
 import datetime as dtq
 
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 2
+
+def depositar(valor):
+            global saldo 
+            saldo += valor
+            global extrato
+            extrato += f"valor depositado: R$ {saldo:.2f}\n"
+
+
+def saque(valor_saque):
+    global LIMITE_SAQUES
+    global saldo
+    global numero_saques
+    
+    if numero_saques != LIMITE_SAQUES:
+        if saldo >= valor_saque:
+            if valor_saque <= limite:
+                saldo -= valor_saque
+                numero_saques += 1
+                extrato = saldo
+                return numero_saques,saldo,extrato
+            else:
+                print("Valor excede quantidade na conta. por favor, realize outra operação.")
+        else:
+            print("Não é possivel sacar mais de R$ 500,00 por vez, por favor, realize outra operação.")
+    else:
+        print("Numero máximo de saques por dia permitido. Por favor, realize outra operação.")
 
 menu = """
 
@@ -11,12 +42,6 @@ menu = """
 
 => """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 10
-
 while True:
 
     print(menu)
@@ -26,28 +51,19 @@ while True:
     if opcao == "d":
 
         valor = float(input("Digite o valor que deseja depositar à conta: "))
+
         if valor > 0:
-            saldo += valor
-            extrato += f"valor depositado: R$ {valor:.2f}\n"
+            depositar(valor)
+            
         else:
             print("Valor negativo. Por favor, realize nova operação e deposite ao digitar valores positivos.")
+
     
     elif opcao == "s":
+       print("Valor maximo de saque unitário: R$ 500,00")
+       valor_saque = int(input("Por favor, digite o valor que deseje sacar: "))
+       saque(valor_saque)
 
-        print("Valor maximo de saque unitário: R$ 500,00")
-        saque = int(input("Por favor, digite o valor que deseje sacar: "))
-        if saldo >= saque:
-            if numero_saques != LIMITE_SAQUES:
-                if saque <= 500:
-                    saldo -= saque
-                    numero_saques += 1
-                else:
-                    print("Valor excede quantidade na conta. por favor, realize outra operação.")
-            else:
-                print("Não é possivel sacar mais de R$ 500,00 por vez, por favor, realize outra operação.")
-
-        else:
-            print("Numero máximo de saques por dia permitido. Por favor, realize outra operação.")
     elif opcao == "e":
         print("################Extrato#########")
         if(extrato == ""):
