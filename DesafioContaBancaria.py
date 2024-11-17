b7833d9a1787ca6d372ce085e4c894c5aaecffdf
Yago Gama <yagoggb97@gmail.com>
2024-11-17 17:23:16 -0300

    Adicionado data e hora dos saques em extrato

--- DesafioContaBancaria.py
+++ DesafioContaBancaria.py
@@ -3,29 +3,42 @@ import datetime as dtq
 
 saldo = 0
 limite = 500
-extrato = ""
+extrato_saldo = ""
 numero_saques = 0
-LIMITE_SAQUES = 2
+LIMITE_SAQUES = 9
+Data_Saque = []
+
 
 def depositar(valor):
+
             global saldo 
             saldo += valor
-            global extrato
-            extrato += f"valor depositado: R$ {saldo:.2f}\n"
+            global extrato_saldo
+            extrato_saldo += f"valor depositado: R$ {saldo:.2f}\n"
 
 
 def saque(valor_saque):
+
     global LIMITE_SAQUES
     global saldo
     global numero_saques
-    
+    global Data_Saque
+
+
     if numero_saques != LIMITE_SAQUES:
+
         if saldo >= valor_saque:
+
             if valor_saque <= limite:
+
                 saldo -= valor_saque
                 numero_saques += 1
-                extrato = saldo
-                return numero_saques,saldo,extrato
+                extrato_saldo = saldo
+                Registro = datetime.now().strftime('%d/%m/%y %H:%M:%S')
+                
+                Data_Saque.append(f"{Registro} " + f"R$ {valor_saque:.2f}")
+
+                return numero_saques,saldo,extrato_saldo
             else:
                 print("Valor excede quantidade na conta. por favor, realize outra operação.")
         else:
@@ -34,13 +47,21 @@ def saque(valor_saque):
         print("Numero máximo de saques por dia permitido. Por favor, realize outra operação.")
 
 def Extrato():
+
     global extrato
     print("################Extrato#########")
-    if(extrato == ""):
-        print("Nenhum deposito realizado.")
+    if(extrato_saldo == ""):
+        print("Nenhum deposito ou saque realizado.")
     else:
-        print(extrato)
-    return 
+
+        print(extrato_saldo)
+        
+        print()
+
+        for d in Data_Saque:
+            print(d)
+  
+    
 menu = """
 
 [d] Depositar
@@ -68,6 +89,7 @@ while True:
 
     
     elif opcao == "s":
+        
        print("Valor maximo de saque unitário: R$ 500,00")
        valor_saque = int(input("Por favor, digite o valor que deseje sacar: "))
        saque(valor_saque)
